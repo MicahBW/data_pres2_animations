90e0ac77fd6b1f171193a3ccfebd2d6d7ba6a1f0
Micah Byrne Wolfsohn <m.b.wolfsohn@gmail.com>
2020-11-14 18:38:45 -0600

    2

--- try2.py
+++ try2.py
@@ -1,10 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import random
 
 
 # plt.plot(1,1, "ro")
 # plt.show()
+
+X, Y = 0, 1
+K = 2
 
 
 def get_data(mu, sigma, length):
@@ -13,24 +17,29 @@ def get_data(mu, sigma, length):
         y = np.random.normal(loc=mu[-1], scale=sigma[-1], size = length)
         return [x,y]
 
-a = get_data((1, 1), (.1, .1), 10)
-b = get_data((3,3), (.3, .3) , 10)
+def euclid(p0, p1) :
+    dx = p0[X] - p1[X]
+    dy = p0[Y] - p1[Y]
+    return sqrt(dx ** 2 + dy ** 2)
+
+
+a = get_data((1, 1), (.5, .5), 10)
+b = get_data((8,8), (1, 1) , 10)
+
+both = np.concatenate((a, b), axis = 1)
+
+
 
 plt.plot(a[0], a[1], "ro")
-plt.plot(b[0], b[1], "ro")
+plt.plot(b[0], b[1], "bo")
+# plt.plot(both[0], both[1], "bx")
+# plt.xlim(0,10)
+# plt.ylim(0,10)
+
 plt.show()
 
 
+# Forgy initialization
 
 
-"""
-def get_data(mu, sigma, length):
-    def convert(list):
-        return tuple(i for i in list)
-    a = np.random.normal(loc=mu, scale=sigma, size=(length, 2))
-    ret = []
-    for element in a:
-        ret.append(convert(element))
-
-    return ret
-"""
+# Choose k random means
