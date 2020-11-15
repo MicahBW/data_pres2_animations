ae89374308eeb9d92a73a98894e69cec85548fbb
Micah Byrne Wolfsohn <m.b.wolfsohn@gmail.com>
2020-11-14 20:27:01 -0600

    Working with one iteration

--- try2.py
+++ try2.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random
 import sys
+import math
 
 
 # plt.plot(1,1, "ro")
 # plt.show()
 
-X, Y = 0, 1
+X, Y, ASSIGMENT = 0, 1, 2
 K = 2
 FLOAT_MAX = sys.float_info.max
 
@@ -23,7 +24,7 @@ def get_data(mu, sigma, length):
 def euclid(p0, p1) :
     dx = p0[X] - p1[X]
     dy = p0[Y] - p1[Y]
-    return sqrt(dx ** 2 + dy ** 2)
+    return math.sqrt(dx ** 2 + dy ** 2)
 
 def graph_format(data):
     # Usage: plt.plot(graph_format(data)[X],graph_format(data)[Y],'or')
@@ -39,42 +40,56 @@ data = np.concatenate((a, b), axis = 0)
 
 print data
 
-plt.plot(graph_format(a)[X],graph_format(a)[Y],'ro')
-plt.plot(graph_format(b)[X],graph_format(b)[Y],'bo')
-plt.show()
-# plt.plot(both[0], both[1], "bx")
+# plt.plot(graph_format(a)[X],graph_format(a)[Y],'ro')
+# plt.plot(graph_format(b)[X],graph_format(b)[Y],'bo')
+plt.plot(graph_format(data)[X],graph_format(data)[Y],'bo')
+# plt.show()
 # plt.xlim(0,10)
 # plt.ylim(0,10)
 
 #plt.show()
 
 
-# d_to_concat = np.concatenate((all_d_sets, [-1] ), axis = 1)
+
+# Forgy initialization
+
+
+# Choose k random means
+initial_means_indexes = random.sample(range(0, len(data) - 1), K)
+means = []
+for i in initial_means_indexes:
+    means.append(data[i])
+print means
+
+# for point in data:
+#     point.append(-1)
+# print data
+
+
+assignments = np.array([[-1]] * len(data))
+dws = np.concatenate((data, assignments), axis = 1) # defines the data with assignments
+
+
+
+
+
+
+# We're going to need to do the following every time
+for point in dws :
+    min_mean = FLOAT_MAX
+    for i in range(0, len(means)):
+        mean = means[i]
+        distance = euclid((point[X], point[Y]), mean)
+        if distance < min_mean:
+            min_mean = distance
+            point[ASSIGMENT] = i
+
+
+
+print dws
+
+for point in dws :
+    if point[ASSIGMENT] == 1:
+        plt.plot(point[X], point[Y], "ro")
+plt.show()
 #
-# data = all_d_sets.T
-#
-# # Forgy initialization
-#
-#
-# # Choose k random means
-# initial_means_indexes = random.sample(range(0, data.size - 1), K)
-# means = []
-# for i in initial_means_indexes:
-#     means.append(data[i])
-# print means
-# assignments = [-1]*data.size
-#
-#
-#
-# # We're going to need to do the following every time
-# for point in datas :
-#     min_mean = FLOAT_MAX
-#     for mean in means:
-#         distance = euclid(point, mean)
-#         if distance < min_mean:
-#             min_mean = distance
-#             assignments
-#
-#
-#
-# #
