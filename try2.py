c2d0d139ffb4d7ba6bbe9b41082f33551614f5e7
Micah Byrne Wolfsohn <m.b.wolfsohn@gmail.com>
2020-11-14 19:35:57 -0600

    Now it is working with the formatter

--- try2.py
+++ try2.py
@@ -18,32 +18,38 @@ def get_data(mu, sigma, length):
     if isinstance(mu, tuple) and isinstance(sigma, tuple):
         x = np.random.normal(loc=mu[0], scale=sigma[0], size = length)
         y = np.random.normal(loc=mu[-1], scale=sigma[-1], size = length)
-        return [x,y]
+        return np.array([x,y]).T
 
 def euclid(p0, p1) :
     dx = p0[X] - p1[X]
     dy = p0[Y] - p1[Y]
     return sqrt(dx ** 2 + dy ** 2)
 
+def graph_format(data):
+    # Usage: plt.plot(graph_format(data)[X],graph_format(data)[Y],'or')
+    x_val = [i[0] for i in data]
+    y_val = [i[1] for i in data]
+    return [x_val, y_val]
+
 
 a = get_data((1, 1), (.5, .5), 10)
 b = get_data((8,8), (1, 1) , 10)
 
-all_d_sets = np.concatenate((a, b), axis = 1)
+data = np.concatenate((a, b), axis = 0)
 
+print data
 
-
-plt.plot(a[0], a[1], "ro")
-plt.plot(b[0], b[1], "bo")
+plt.plot(graph_format(a)[X],graph_format(a)[Y],'ro')
+plt.plot(graph_format(b)[X],graph_format(b)[Y],'bo')
+plt.show()
 # plt.plot(both[0], both[1], "bx")
 # plt.xlim(0,10)
 # plt.ylim(0,10)
 
 #plt.show()
 
-print all_d_sets
 
-d_to_concat = np.concatenate((all_d_sets, [-1] ), axis = 1)
+# d_to_concat = np.concatenate((all_d_sets, [-1] ), axis = 1)
 #
 # data = all_d_sets.T
 #
