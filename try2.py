6013c44be5e965c7d33321d171f90ccd739bd5fe
Micah Byrne Wolfsohn <m.b.wolfsohn@gmail.com>
2020-11-16 18:33:38 -0600

    IT WORKS

--- try2.py
+++ try2.py
@@ -10,7 +10,7 @@ import math
 # plt.show()
 
 X, Y, ASSIGMENT = 0, 1, 2
-K = 2
+K = 3
 FLOAT_MAX = sys.float_info.max
 
 
@@ -33,11 +33,13 @@ def graph_format(data):
     return [x_val, y_val]
 
 
-a = get_data((1, 1), (.2, .2), 10)
-b = get_data((5,5), (1, 1) , 10)
-c = get_data((3,3), (3, 3) , 20)
+a = get_data((0,0), (3, 3), 100)
+# b = get_data((9,0), (1, 1) , 10)
+# c = get_data((5,9), (1, 1) , 20)
 
-data = np.concatenate((a, b, c), axis = 0)
+# data = np.concatenate((a, b, c), axis = 0)
+data = a
+
 
 # print data
 
@@ -45,8 +47,8 @@ data = np.concatenate((a, b, c), axis = 0)
 # plt.plot(graph_format(b)[X],graph_format(b)[Y],'bo')
 # plt.plot(graph_format(data)[X],graph_format(data)[Y],'bo')
 # plt.show()
-plt.xlim(0,10)
-plt.ylim(0,10)
+# plt.xlim(0,10)
+# plt.ylim(0,10)
 
 #plt.show()
 
@@ -62,60 +64,21 @@ for i in initial_means_indexes:
     means.append(data[i])
 print means
 
-
-# means = [data[0], data[1]]
-
-
+# clusters = np.array([])
+# for i in range(K):
+#     clusters.append(np.array([]))
 
 assignments = np.array([[-1]] * len(data))
 dws = np.concatenate((data, assignments), axis = 1) # defines the data with assignments
 
-
-
-
-
-# Divide the points into two groups
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-ass_colors = ["bo", "ro"]
+ass_colors = ["b", "r", "m", "k", "g"]
 prev_means = []
-# changes_made = True
+
+
 while True:
-    # We're going to need to do the following every time
-    ps = ""
-    # Assign the points
+
+    # Assignment Step
     for point in dws :
-        ps += str(point[ASSIGMENT])
 
         min_mean = FLOAT_MAX
         for i in range(0, len(means)):
@@ -124,19 +87,26 @@ while True:
             if distance < min_mean:
                 min_mean = distance
                 point[ASSIGMENT] = float(i)
-    # print ps
+
+    # Vizualization Step
+    # Plot the points by their assignment
+    for point in dws :
+        plt.plot(point[X], point[Y], ass_colors[int(point[ASSIGMENT])] + "o") # PERROR: Int conversion
+
+    # And plot the meanss
+    for mean in means:
+        plt.plot(mean[X], mean[Y], "^c")
+    for pm in prev_means:
+        plt.plot(pm[X], pm[Y], "^y")
+    # plt.plot(graph_format(a)[X],graph_format(a)[Y],'gx')
+    plt.show()
 
 
-    # Check if the point assignments changed
-    # changes_made = False
-    # for i in range(len(dws)):
-    #     if dws[i][ASSIGMENT] != prev_dws[i][ASSIGMENT]:
-    #         changes_made = True
-    #         print changes_made
-    #         break
 
 
-    # Recalculate Means
+
+
+    # Update step
     mean_sum_x = mean_sum_y = 0.0
     mean_counter = 0.0
     for i in range(K):
@@ -153,35 +123,14 @@ while True:
 
 
 
-    # Plot the points by their assignment
-    for point in dws :
-        plt.plot(point[X], point[Y], ass_colors[int(point[ASSIGMENT])]) # PERROR: Int conversion
-
-    # And plot the means
-    for mean in means:
-        plt.plot(mean[X], mean[Y], "oc")
-    for pm in prev_means:
-        plt.plot(pm[X], pm[Y], "oy")
-    # plt.plot(graph_format(a)[X],graph_format(a)[Y],'gx')
-    plt.show()
-
     # If the point assigments don't change then that imlies that the means won't have changed
+    print "prev_means"
+    print prev_means
+    print "MEANS:"
+    print means
     if prev_means == means:
-        print prev_means
-        print means
         break
-    prev_means = means
-
-
-
-
+    prev_means = means[:] #PERROR: Ther are now t
 
 
 print dws
-# ass_colors = ["bo", "ro"]
-# for point in dws :
-#     plt.plot(point[X], point[Y], ass_colors[int(point[ASSIGMENT])]) # PERROR: Int conversion
-#
-# plt.plot(graph_format(a)[X],graph_format(a)[Y],'gx')
-# plt.show()
-#
