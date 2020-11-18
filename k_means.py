7efe8132e212524b45e387adf93b804178054fc2
Micah Byrne Wolfsohn <m.b.wolfsohn@gmail.com>
2020-11-18 11:22:11 -0600

    Added support

--- k_means.py
+++ k_means.py
@@ -4,16 +4,13 @@ import matplotlib.animation as animation
 import random
 import sys
 import math
-from datetime import datetime
 
 
-# plt.plot(1,1, "ro")
-# plt.show()
 
 X, Y, ASSIGMENT = 0, 1, 2
-K = 2
+K = 3 # CHANGE
 FLOAT_MAX = sys.float_info.max
-ass_colors = ["b", "r", "m", "k", "g"]
+
 
 
 def get_data(mu, sigma, length):
@@ -21,9 +18,6 @@ def get_data(mu, sigma, length):
         x = np.random.normal(loc=mu[0], scale=sigma[0], size = length)
         y = np.random.normal(loc=mu[-1], scale=sigma[-1], size = length)
         return np.array([x,y]).T
-    else:
-        print "ERROR: Ivalid use of get_data"
-
 
 def euclid(p0, p1) :
     dx = p0[X] - p1[X]
@@ -36,28 +30,18 @@ def graph_format(data):
     y_val = [i[1] for i in data]
     return [x_val, y_val]
 
-## REMEMBER TO CHANGE K
-## TRIANGULAR
-# a = get_data((0,0), (0.5, 0.5), 30)
-# b = get_data((9,0), (0.5, 0.5), 30)
-# c = get_data((5,0), (5, 0.5), 30)
 
-## TWO LINES
-a = get_data((-1,5), (0.01, 3), 30)
-b = get_data((1,5), (0.01, 3), 30)
+a = get_data((0,0), (1.5, 1.5), 30) # CHANGE Usage: (meanx, meany), (varx, vary), count
+b = get_data((9,0), (1.5, 1.5), 30) # CHANGE
+c = get_data((5,9), (1.5, 1.5), 30) # CHANGE
 
-
-data = np.concatenate((a, b), axis = 0)
+data = np.concatenate((a, b, c), axis = 0) # CHANGE
 
 assignments = np.array([[-1]] * len(data))
 dws = np.concatenate((data, assignments), axis = 1) # defines the data with assignments
 
-
+ass_colors = ["b", "r", "m", "k", "g"]
 prev_means = []
-
-plt.xlim(-10, 10)
-plt.ylim(-10, 10)
-
 
 
 
@@ -67,12 +51,14 @@ means = []
 for i in initial_means_indexes:
     means.append(data[i])
 
+# Vizualization Step
 for point in dws :
-    plt.plot(point[X], point[Y],  "ko", markersize=10)
+    plt.plot(point[X], point[Y], "ko")
 for mean in means:
-    plt.plot(mean[X], mean[Y], "^c", markersize=10)
-# plt.savefig("created_graphics/" + datetime.utcnow().strftime('%Y%m%d %H%M%S%f')[:-3] + "START")
+    plt.plot(mean[X], mean[Y], "^c")
 plt.show()
+
+
 
 while True:
 
@@ -89,10 +75,9 @@ while True:
 
     # Vizualization Step
     for point in dws :
-        plt.plot(point[X], point[Y], ass_colors[int(point[ASSIGMENT])] + "o", markersize=10)
+        plt.plot(point[X], point[Y], ass_colors[int(point[ASSIGMENT])] + "o")
     for mean in means:
-        plt.plot(mean[X], mean[Y], "^c", markersize=10)
-    # plt.savefig("created_graphics/" + datetime.utcnow().strftime('%Y%m%d %H%M%S%f')[:-3])
+        plt.plot(mean[X], mean[Y], "^c")
     plt.show()
 
 
@@ -113,13 +98,10 @@ while True:
 
     # Vizualization Step
     for point in dws :
-        plt.plot(point[X], point[Y], ass_colors[int(point[ASSIGMENT])] + "o", markersize=10)
+        plt.plot(point[X], point[Y], ass_colors[int(point[ASSIGMENT])] + "o")
     for mean in means:
-        plt.plot(mean[X], mean[Y], "^c", markersize=10)
-    # plt.savefig("created_graphics/" + datetime.utcnow().strftime('%Y%m%d %H%M%S%f')[:-3] + "U")
+        plt.plot(mean[X], mean[Y], "^c")
     plt.show()
-
-
 
 
     # If the point assigments don't change then that imlies that the means won't have changed
@@ -128,12 +110,6 @@ while True:
     print "MEANS:"
     print means
     if prev_means == means:
-        for point in dws :
-            plt.plot(point[X], point[Y], ass_colors[int(point[ASSIGMENT])] + "o", markersize=10)
-        for mean in means:
-            plt.plot(mean[X], mean[Y], "^c", markersize=10)
-        plt.savefig("created_graphics/" + datetime.utcnow().strftime('%Y%m%d %H%M%S%f')[:-3] + "FINAL")
-        plt.show()
         break
     prev_means = means[:] #PERROR: Ther are now t
 
